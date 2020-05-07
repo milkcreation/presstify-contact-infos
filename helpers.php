3e72de288a7a3915e3423fb1c5a8d1f4eccef979
Jordy Manner <jordy.manner@milkcreation.fr>
2020-05-07 08:19:19 +0000

    contact-infos -- 2.0 -- Dépôt initial

---
+++ helpers.php
@@ -0,0 +1,20 @@
+<?php declare(strict_types=1);
+
+use tiFy\Plugins\ContactInfos\ContactInfos;
+
+if (!function_exists('contact_infos')) {
+    /**
+     * Récupération de la liste des informations de contact ou d'un attributs particulier.
+     *
+     * @param string|null $key
+     * @param mixed $default
+     *
+     * @return mixed
+     */
+    function contact_infos(?string $key = null, $default = null)
+    {
+        $infos = ContactInfos::instance()->infos();
+
+        return is_null($key) ? $infos->all() : $infos->get($key, $default);
+    }
+}
